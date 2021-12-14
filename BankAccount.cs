2f5b1220ecf98e08212796f26a1feedbf27bc896
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2021-12-14 13:54:14 -0800

    BankEncapsulationExercise

---
+++ BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+        
+    }
+    
+    
+ 
+    
+    
+    
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aaronsAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            aaronsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thankyou Your balance is now {aaronsAccount.GetBalance()}");
+        }
+    }
+}
